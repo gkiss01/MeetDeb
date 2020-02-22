930a10fa2117863f10a36b49596deeb154e86a65
gkiss01 <gergokiss05@gmail.com>
2020-02-22 19:17:59 +0100

    Esemény részletek megjelenítése BottomSheet-en javítva

--- app/src/main/java/com/gkiss01/meetdeb/screens/DetailsBottomSheetFragment.kt
+++ app/src/main/java/com/gkiss01/meetdeb/screens/DetailsBottomSheetFragment.kt
@@ -1,33 +1,16 @@
 package com.gkiss01.meetdeb.screens
 
-import android.graphics.Color
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.andrefrsousa.superbottomsheet.SuperBottomSheetFragment
 import com.gkiss01.meetdeb.MainActivity
 import com.gkiss01.meetdeb.R
 import com.gkiss01.meetdeb.data.Event
 import kotlinx.android.synthetic.main.details_fragment_bottomsheet.*
 import org.threeten.bp.format.DateTimeFormatter
-
-class DetailsDialogFragment : Fragment() {
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.details_fragment, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val event = arguments!!.getSerializable("event") as Event
-
-        val bottomSheet = DetailsBottomSheetFragment(event)
-        bottomSheet.show(childFragmentManager, "DetailsBottomSheetFragment")
-    }
-}
 
 class DetailsBottomSheetFragment(private val event: Event): SuperBottomSheetFragment() {
 
@@ -53,4 +36,7 @@ class DetailsBottomSheetFragment(private val event: Event): SuperBottomSheetFrag
     }
 
     override fun getCornerRadius() = context!!.resources.getDimension(R.dimen.bottomsheet_corner_radius)
+
+    override fun getPeekHeight() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 105F,
+        context!!.resources.displayMetrics).toInt()
 }
