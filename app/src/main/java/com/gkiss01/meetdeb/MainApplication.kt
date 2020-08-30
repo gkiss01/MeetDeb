8c40decc9be17bcd75cf95b79c4d2505e8c91833
gkiss01 <kiss.gergo@codeyard.eu>
2020-08-30 14:36:12 +0200

    Kotlin 1.4 és függőség verziók frissítve

--- app/src/main/java/com/gkiss01/meetdeb/MainApplication.kt
+++ app/src/main/java/com/gkiss01/meetdeb/MainApplication.kt
@@ -10,13 +10,14 @@ import com.gkiss01.meetdeb.viewmodels.participantsModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            androidLogger(Level.ERROR)
             androidContext(this@MainApplication)
             modules(listOf(networkModule, restModule, activityModule, eventsModule, datesModule, participantsModule, createModule))
         }
